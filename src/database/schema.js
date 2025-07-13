53fed66033c1a46f071328c7e738c2020b724dcc
Giesen-Carl <70052513+Giesen-Carl@users.noreply.github.com>
2025-07-13 12:39:04 +0200

    zwischenstand

--- src/database/schema.js
+++ src/database/schema.js
@@ -2,10 +2,36 @@ import { createTableIfNotExists, dropAll } from "./queries.js";
 import datatypes from "./datatypes.js";
 
 const schema = {
+    User: {
+        uuid: datatypes.UUID,
+        username: datatypes.STRING,
+        role: datatypes.STRING,
+    },
     Auth: {
         uuid: datatypes.UUID,
         password: datatypes.STRING,
     },
+    Bestellung: {
+        username: datatypes.STRING,
+        cocktailName: datatypes.STRING,
+        status: datatypes.STRING,
+        timestamp: datatypes.DATETIME,
+    },
+    Cocktail: {
+        name: datatypes.STRING,
+        category: datatypes.STRING,
+        price: datatypes.DOUBLE,
+        description: datatypes.STRING,
+    },
+    Igredient: {
+        name: datatypes.STRING,
+        available: datatypes.BOOL,
+    },
+    CocktailIngredient: {
+        cocktailId: datatypes.INT,
+        ingredientId: datatypes.INT,
+        menge: datatypes.STRING,
+    }
 }
 
 

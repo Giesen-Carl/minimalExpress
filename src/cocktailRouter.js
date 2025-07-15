ede278e8619eac8795a5ee8d9f8629ad6d03088c
gies <karl.giesen@soptim.de>
2025-07-15 10:58:13 +0200

    yay

--- src/cocktailRouter.js
+++ src/cocktailRouter.js
@@ -4,7 +4,7 @@ import Cocktail from './database/model/cocktailModel.js';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import { redirect } from './auth_router.js';
-import { getAllIngredients, getCocktailByName } from './database/queries.js';
+import { createObject, getAllIngredients, getCocktailByName } from './database/queries.js';
 
 const cocktailRouter = express.Router();
 cocktailRouter.use(cookieParser());
@@ -27,11 +27,35 @@ cocktailRouter.route('/cocktails')
             if (cocktail) {
                 throw new Error('Es existiert bereits ein Cocktail mit diesem Namen.')
             }
+            const ingredients = JSON.parse(body.ingredients || '[]');
+            if (ingredients.length === 0) {
+                throw new Error('Es muss mindestens eine Zutat angegeben werden.');
+            }
+            for (const ingredient of ingredients) {
+                if (!ingredient.ingredient) {
+                    throw new Error('Alle Zutaten müssen einen Namen haben.');
+                }
+            }
+            const db_ingredients = await getAllIngredients();
+            for (const ingredient of ingredients) {
+                if (!db_ingredients.map(ing => ing.ingredient_name).includes(ingredient.ingredient)) {
+                    throw new Error(`Die Zutat "${ingredient.ingredient}" existiert nicht.`);
+                }
+            }
             await createObject('cocktail', {
                 cocktail_name: body.cocktail_name,
                 category: body.category,
                 price: body.price,
             });
+            const cocktail_id = (await getCocktailByName(body.cocktail_name)).id;
+            for (const ingredient of ingredients) {
+                const ingredient_id = db_ingredients.find(db_ing => db_ing.ingredient_name === ingredient.ingredient).id;
+                await createObject('cocktail_ingredient', {
+                    cocktail_id: cocktail_id,
+                    ingredient_id: ingredient_id,
+                    menge: ingredient.menge,
+                });
+            }
             res.redirect('/');
         } catch (error) {
             const selectedIngredients = JSON.parse(body.ingredients || '[]').map(ing => ing.ingredient);
